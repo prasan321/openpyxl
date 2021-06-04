07d5bfcb96ec9e83c3f3c81cab66c528374841e9
Prasan <42831073+prasan321@users.noreply.github.com>
2021-06-04 22:30:40 +0800

    new

--- writeExcel.py
+++ writeExcel.py
@@ -7,6 +7,6 @@ sheet['A1'] = 'Hello'
 sheet['B1'] = 'world!!!'
 
 #workbook.save(filename='hello_world.xlsx')
-workbook.save(filename='hello_world.csv')
+workbook.save(filename='hello_worlds.csv')
 print('succesfully saved !!! ')
 
